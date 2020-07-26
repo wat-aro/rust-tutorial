11abbe2bba76a16c0b5bcf8e1e161b666b39a05b
wat-aro <kazutas1008@gmail.com>
2020-07-26 22:23:04 +0900

    Finish enums

---
+++ enums/src/main.rs
@@ -0,0 +1,19 @@
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("Hello, world!");
+}
