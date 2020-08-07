bd45ea8ea03b5c3d720186cb2a44e74e3807e20b
wat-aro <kazutas1008@gmail.com>
2020-08-08 00:20:26 +0900

    Finish my_crate

---
+++ my_crate/src/lib.rs
@@ -0,0 +1,18 @@
+//! # My Crate
+//! 
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, my_crate::add_one(5));
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
