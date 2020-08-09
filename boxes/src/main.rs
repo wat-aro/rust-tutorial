5958bf3a895be0f88c86337aa39b7309df96836c
wat-aro <kazutas1008@gmail.com>
2020-08-09 12:17:51 +0900

    Finish deref

--- boxes/src/main.rs
+++ boxes/src/main.rs
@@ -3,6 +3,8 @@ use List::{Cons, Nil};
 fn main() {
     let list = List::cons(1, List::cons(2, List::cons(3, Nil)));
     println!("{:?}", list);
+    println!("{:?}", List::cdr(&list));
+    println!("{:?}", List::cdr(List::cdr(&list)));
 }
 
 #[derive(Debug)]
@@ -11,8 +13,17 @@ enum List {
     Nil,
 }
 
+use std::ops::Deref;
+
 impl List {
     fn cons(head: i32, tail: List) -> List {
         Cons(head, Box::new(tail))
+    }
+
+    fn cdr(list: &List) -> &List {
+        match list {
+            Nil => &Nil,
+            Cons(_, rest) => rest.deref()
+        }
     }
 }
